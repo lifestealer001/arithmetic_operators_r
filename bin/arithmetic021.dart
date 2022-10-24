16fc4f6fb5314ba191a2ec50273e2a9274de9172
lifestealer001 <abrormurtazaqulov@gmail.com>
2022-10-24 16:33:27 +0500

    777

--- bin/arithmetic021.dart
+++ bin/arithmetic021.dart
@@ -1,7 +1,7 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 double func(int a, int b,int c){
-    return (2*b*b)/(a+c*c);
+    return 2*b*b/(a+(c*c));
 }
 void main() {
     print(func(1,11,3));
--- bin/arithmetic035.dart
+++ bin/arithmetic035.dart
@@ -1,7 +1,7 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 import 'dart:math';
-double func(int a,int b){
+double func(num a,num b){
     return pow(a, b)*pow(b, a)*((a+b)/a*b);
 }
 void main() {
