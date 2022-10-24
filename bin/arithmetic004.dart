1920e36025e1fa1318f103cc22ce725936bb2ef6
lifestealer001 <abrormurtazaqulov@gmail.com>
2022-10-24 16:12:42 +0500

    ssss

--- bin/arithmetic004.dart
+++ bin/arithmetic004.dart
@@ -1,7 +1,7 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 int func(int a,int b){
-    return b/~a;
+    return a~/b;
 }
 void main() {
     print(func(12,48));
